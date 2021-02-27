f3fcd32728843f8b4be3859b6bd5aa98d4260b1a
Mark | Microtick <409166+mark-microtick@users.noreply.github.com>
2021-02-27 09:23:10 -0700

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     Roadmap: ['roadmap', 'tokenlicense'],
+    Stargate: ['stargate'],
     Whitepaper: ['overview', 'pricediscovery', 'oracles', 'consensus', 'futures'],
     "Frequently Asked Questions": ['faq', 'license'],
     "How To": ['runvalidator']
